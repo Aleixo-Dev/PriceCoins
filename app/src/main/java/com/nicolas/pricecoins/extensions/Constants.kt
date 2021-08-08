ae5de4cc1e1fad3a5d88cef5ee01d6fd05c01d7a
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-08-08 20:48:05 -0300

    classes utils para o app

---
+++ app/src/main/java/com/nicolas/pricecoins/extensions/Constants.kt
@@ -0,0 +1,7 @@
+package com.nicolas.pricecoins.extensions
+
+object Constants {
+    const val BASE_URL = "https://api.hgbrasil.com/"
+    const val ENDPOINT = "finance"
+    const val KEY = "726225"
+}
